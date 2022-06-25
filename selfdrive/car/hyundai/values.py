f6934efc2fd41a7b7a99bb33531b289be1a59730
shake777 <69850594+shake777@users.noreply.github.com>
2022-06-25 13:56:24 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -13,7 +13,7 @@ class CarControllerParams:
     else:
       self.STEER_MAX = 409
 
-    self.STEER_DELTA_UP = 4
+    self.STEER_DELTA_UP = 3
     self.STEER_DELTA_DOWN = 7
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
