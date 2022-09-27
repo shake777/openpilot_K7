ab5b5427e00e0ad172dc99e6e5901ce6e8a65e5b
shake777 <69850594+shake777@users.noreply.github.com>
2022-09-27 13:16:29 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -14,7 +14,7 @@ class CarControllerParams:
 
   def __init__(self, CP):
     self.STEER_MAX = 409
-    self.STEER_DELTA_UP = 6
+    self.STEER_DELTA_UP = 5
 
     if CP.carFingerprint in [CAR.KONA_HEV, CAR.KONA_EV]:
       self.STEER_DELTA_DOWN = 7
