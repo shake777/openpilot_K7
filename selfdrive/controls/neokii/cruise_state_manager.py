04c68b6698cc326c6b8a3eba9db96720721d08d9
boxkon <ghkdxo14@gmail.com>
2022-12-12 08:42:36 +0900

    clean up CRZ Auto-Set

--- selfdrive/controls/neokii/cruise_state_manager.py
+++ selfdrive/controls/neokii/cruise_state_manager.py
@@ -53,7 +53,6 @@ class CruiseStateManager:
     self.prev_brake_pressed = False
 
     # CRZ AUTO-SET by Tenesi
-    self.prev_cancel = False
     self.cruise_auto = False
 
     self.is_metric = Params().get_bool('IsMetric')
@@ -100,13 +99,10 @@ class CruiseStateManager:
     CS.cruiseState.available = self.available
 
     # CRZ AUTO-SET by Tenesi
-    ascc_auto_set = (CS.vEgoCluster * CV.MS_TO_KPH > 30) and CS.gasPressed
+    ascc_auto_set = (CS.vEgoCluster * CV.MS_TO_KPH > 20) and CS.gasPressed
     # 1회 수동 크루즈 인게이지 후 가속페달 작동시... 앞차가 있는 경우에 자동 크루즈셋및 시속 30이상시 이전 속도로 셋한다..
-    if not self.prev_cancel and self.cruise_auto: # 캔슬버튼을 누르지 않았다면...초기 false상태에서 오토셋은 작동한다..
-      if ascc_auto_set and not self.enabled: 	  # 조건이 맞다면 크루즈셋을 셋팅하고
-        self.enabled = True 				 	  #오토셋 작동했다는 조건을 True로 만들어준다..
-    else: 									 	  #캔슬버튼을 눌러서 True상태라면 크루즈셋은 취소시키고 오토셋 작동했다는 조건은 False로 만든다..
-      self.enabled = False
+    if ascc_auto_set and self.cruise_auto and not self.enabled: # 캔슬버튼을 누르지 않았다면...초기 false상태에서 오토셋은 작동한다..
+      self.enabled = True
 
     if cruise_state_control:
       CS.cruiseState.enabled = self.enabled
@@ -162,7 +158,6 @@ class CruiseStateManager:
         elif btn == ButtonType.decelCruise:
           v_cruise_kph -= v_cruise_delta - -v_cruise_kph % v_cruise_delta
     else:
-
       if not self.btn_long_pressed:
         if btn == ButtonType.decelCruise and not self.enabled:
           self.enabled = True
@@ -185,12 +180,7 @@ class CruiseStateManager:
 
     if btn == ButtonType.cancel:
       self.enabled = False
-      self.cruise_auto = False
-      # # CRZ AUTO-SET by Tenesi
-      # if self.prev_cancel:       #최초 초기상태는 False이고 캔슬로 입력받으면 True로 전환됨..
-      #   self.prev_cancel = False #다시 버튼이 누르면 False가 되고 다시 버튼을 누르면 True가 됨
-      # else:
-      #   self.prev_cancel = True
+      self.cruise_auto = False # CRZ AUTO-SET by Tenesi
 
     v_cruise_kph = clip(round(v_cruise_kph, 1), V_CRUISE_MIN_CRUISE_STATE, V_CRUISE_MAX)
     self.speed = v_cruise_kph * CV.KPH_TO_MS
