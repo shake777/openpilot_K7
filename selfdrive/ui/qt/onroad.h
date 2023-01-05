63a64b1919f2a17e99ccfe42c7c6b1424c91a33c
shake777 <jungsanpark@gmail.com>
2023-01-05 19:17:16 +0900

    magenta

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -44,6 +44,7 @@ protected:
   void updateFrameMat() override;
   void drawLaneLines(QPainter &painter, const UIState *s);
   void drawLead(QPainter &painter, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd, bool is_radar);
+  inline QColor magentaColor(int alpha = 255) { return QColor(255, 0, 255, alpha); }
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
   inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
